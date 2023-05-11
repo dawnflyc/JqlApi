7f501c09311118203670c2886e573c292bd985ca
dawnflyc <dawnflyc@gmail.com>
2023-05-11 14:33:22 +0800

    0.1.1.18.2 添加config提示

--- src/main/java/com/dawnflyc/jqlapi/config/ConfigManage.java
+++ src/main/java/com/dawnflyc/jqlapi/config/ConfigManage.java
@@ -2,6 +2,8 @@ package com.dawnflyc.jqlapi.config;
 
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.InvocationHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -10,7 +12,7 @@ import java.util.Locale;
 //配置管理器
 public class ConfigManage {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(ConfigManage.class);
     private static Config config;
 
     public static Config getConfig() {
@@ -29,6 +31,8 @@ public class ConfigManage {
                     Object value = field.get(currentConfig);
                     if(value==null){
                         method.invoke(currentConfig,args);
+                    }else {
+                        logger.error("config只能修改一次");
                     }
                 }
                 if (method.getName().startsWith("get")){
