2cb8555783adbcd42042af7549a4522c0e23da58
dawnflyc <dawnflyc@gmail.com>
2023-05-11 14:21:07 +0800

    0.1.1.18 时间日志打印完成

--- src/main/java/com/dawnflyc/jqlapi/config/DefaultConfig.java
+++ src/main/java/com/dawnflyc/jqlapi/config/DefaultConfig.java
@@ -3,7 +3,6 @@ package com.dawnflyc.jqlapi.config;
 public class DefaultConfig extends Config {
 
     public DefaultConfig() {
-        this.setPrintForgetRemind(false);
         this.setPrintRuntime(false);
     }
 }
